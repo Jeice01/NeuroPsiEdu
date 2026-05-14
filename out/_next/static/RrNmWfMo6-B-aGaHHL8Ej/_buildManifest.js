48f540dc280e9c23b53a65a7978889a075a27469
jeice01 <jeice.santos@gmail.com>
2026-05-14 18:50:31 -0300

    Fix static export config

---
+++ out/_next/static/RrNmWfMo6-B-aGaHHL8Ej/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/3fa41ce8740f0aa0.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ out/_next/static/chunks/3fa41ce8740f0aa0.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/3580f1b1d0e5d8f3.js",
+  "static/chunks/turbopack-a3297c4bf3405ed4.js"
+])
